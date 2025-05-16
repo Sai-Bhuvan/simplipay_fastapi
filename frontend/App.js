d5d0755b02e28e66b2ef27e2720cd5308ae39724
Harshith C S <107236868+harshith082003@users.noreply.github.com>
2025-05-16 20:52:56 +0530

    navigation changes

--- frontend/App.js
+++ frontend/App.js
@@ -1,17 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Text} from '@ui-kitten/components';
 import Signup from './screens/signup';
+import { ApplicationProvider } from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
 import SignIn from './screens/signin';
-import HomePage from './screens/HomePage';
-import FaceCaptureScreen from './screens/Camera';
 
 
-export default () => (
-  <ApplicationProvider {...eva} theme={eva.dark}>
-    <View>
-      
-    </View>
-  </ApplicationProvider>
-);
+export default function App() {
+
+  const [currentPage, setCurrentPage] = useState("SignUp")
+
+  const handlechangePage = (newPage) => {
+    setCurrentPage(newPage);
+  }
+
+  const renderPage = () => {
+    switch(currentPage){
+      case "SignUp": return <Signup onPageChange={handlechangePage}/>
+      case "SignIn": return <SignIn onPageChange={handlechangePage}/>
+      case "HomePage": return <HomePage onPageChange={handlechangePage}/>
+    }
+  }
+
+  return(
+    <ApplicationProvider icons={EvaIconsPack} {...eva} theme={eva.dark}>
+     
+      <View style = {{flex: 1, }}>
+        {renderPage()}
+
+      </View>
+    </ApplicationProvider>
+    
+  );
+  
+}
+
+  
